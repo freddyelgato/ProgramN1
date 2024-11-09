8fde43be41978addddde2ad63c081f04a8d08b58
freddyelgato <freddymoreirala@gmail.com>
2024-11-09 18:45:48 -0500

    feat: :sparkles: Create Dockerfile and relative file for building
    The Dockerfile will help us to create the image

--- PaginaWeb_C#/obj/Debug/net8.0/PaginaWeb_C#.AssemblyInfo.cs
+++ PaginaWeb_C#/obj/Debug/net8.0/PaginaWeb_C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaginaWeb_C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98e32a5570968fbfb2fca932569b578fff8856ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3046f479bcb2a17585015c7a472d9642ded54c09")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaginaWeb_C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaginaWeb_C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
